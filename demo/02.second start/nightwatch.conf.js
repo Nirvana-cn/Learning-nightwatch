359861bee6dd34498f37a02ced1a11723203953a
Alliance <574014131@qq.com>
2019-01-31 18:26:56 +0800

    --

---
+++ demo/02.second start/nightwatch.conf.js
@@ -0,0 +1,8 @@
+const chromedriver = require("chromedriver");
+const geckodriver = require("geckodriver");
+
+module.exports = (function(settings) {
+    settings.test_workers = false;
+    settings.webdriver.server_path = geckodriver.path;
+    return settings;
+})(require('./nightwatch.json'));
